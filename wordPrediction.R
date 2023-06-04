aaa258ea65516eb21b0c849072ec7eb62e528988
pimikeymike <mgar_datascience@protonmail.com>
2023-06-04 17:48:36 -0400

    updates

--- wordPrediction.R
+++ wordPrediction.R
@@ -78,18 +78,39 @@ filedatareader(zip_test)
 
 
 filelist <- c("en_US.blogs.txt", "en_US.twitter.txt", "en_US.news.txt")
+filelist3 <- eval(parse(text=filelist))
+filelist2 <- c(en_US.blogs.txt, en_US.twitter.txt, en_US.news.txt)
+ds <- c()
+for (i in filelist) {
+  var = eval(parse(text=i))
+  ds <- c(var)
+}
+
+
+
 gen_stats <- function(txtlist, multiplier) {
   #   899,288 docs blogs
   # 2,360,148 docs (largest collection in terms of documents) tweets
   #    77,259 docs news
-  set.seed( 1984 ); ds.blogs  <- sample(en_US.blogs.txt,   0.2 * length(en_US.blogs.txt) ) # around 90k entries
-  set.seed( 1984 ); ds.tweets <- sample(en_US.twitter.txt, 0.2 * length(en_US.twitter.txt))
-  set.seed( 1984 ); ds.news   <- sample(en_US.news.txt,    0.2 * length(en_US.news.txt))
+  for (i in txtlist){
+    var = eval(parse(text = i))
+    set.seed(1984); assign(paste0("ds.",i), sample(var,   0.2 * length(var)), envir = .GlobalEnv)
+    #set.seed( 1984 ); ds.blogs  <- sample(en_US.blogs.txt,   0.2 * length(en_US.blogs.txt) ) # around 90k entries
+    #set.seed( 1984 ); ds.tweets <- sample(en_US.twitter.txt, 0.2 * length(en_US.twitter.txt))
+    #set.seed( 1984 ); ds.news   <- sample(en_US.news.txt,    0.2 * length(en_US.news.txt))
+  }
+  # blending texts together
+  ds <<- eval(parse(text=filelist))
+  
+  #set.seed(1984); assign(paste0("ds.","en_US.blogs.txt"), sample(en_US.blogs.txt,   0.2 * length(en_US.blogs.txt)), envir = .GlobalEnv)
+  #set.seed( 1984 ); ds.blogs  <- sample(en_US.blogs.txt,   0.2 * length(en_US.blogs.txt) ) # around 90k entries
+  #set.seed( 1984 ); ds.tweets <- sample(en_US.twitter.txt, 0.2 * length(en_US.twitter.txt))
+  #set.seed( 1984 ); ds.news   <- sample(en_US.news.txt,    0.2 * length(en_US.news.txt))
   
   # blending texts together
-  ds <- c(ds.blogs, ds.tweets, ds.news)
+  #ds <- c(ds.blogs, ds.tweets, ds.news)
   
-  hist( stri_count_words(ds), breaks=30, col=rainbow(50), main = paste("Number of words distribution for", prettyNum(length(ds), scientific=FALSE, big.mark=","), "documents" ))
+  hist(stri_count_words(ds), breaks=30, col=rainbow(50), main = paste("Number of words distribution for", prettyNum(length(ds), scientific=FALSE, big.mark=","), "documents" ))
   
   # 246564
   length(ds)
@@ -103,12 +124,12 @@ gen_stats <- function(txtlist, multiplier) {
   
   # calculate how much memory each object requires, and list the largest 10
   tail( sort( sapply(ls(), function(x) object.size(get(x)) ) ) , 10)
-  rm(en_US.blogs.txt)
-  rm(en_US.news.txt)
-  rm(en_US.twitter.txt)
-  rm(ds.blogs)
-  rm(ds.news)
-  rm(ds.tweets)
+  #rm(en_US.blogs.txt)
+  #rm(en_US.news.txt)
+  #rm(en_US.twitter.txt)
+  #rm(ds.blogs)
+  #rm(ds.news)
+  #rm(ds.tweets)
   gc()
 }
 
