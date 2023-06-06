159d1b754dc36bfaab3b90cf70d7e2ff31cb5cba
pimikeymike <mgar_datascience@protonmail.com>
2023-06-06 16:19:56 -0400

    udpdates

--- wordPrediction.R
+++ wordPrediction.R
@@ -38,7 +38,6 @@ zipdownloader <- function(source_url,zip_file, list = NULL){
     unlink(tempFile)
     }
 
-zx <- zipdownloader("https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip","Coursera-SwiftKey.zip", list = T)
 
 ziphandler <- function(x){
   # look at the source files
@@ -58,7 +57,6 @@ ziphandler <- function(x){
   return(zip_files)
 }
 
-zip_test <- ziphandler(zx)
 
 filedatareader <- function(file_df){
   # this section reads all the files in memory 
@@ -76,11 +74,6 @@ filedatareader <- function(file_df){
     file_content <- NULL
   }
 }
-
-filedatareader(zip_test)
-
-
-filelist <- c("en_US.blogs.txt", "en_US.twitter.txt", "en_US.news.txt")
 
 cleanObjects <- function(objlist) {
   for (i in objlist) {
@@ -125,9 +118,6 @@ gen_stats <- function(txtlist, multiplier) {
   #return(hist(stri_count_words(ds), breaks=30, col=rainbow(50), main = paste("Number of words distribution for", prettyNum(length(ds), scientific=FALSE, big.mark=","), "documents" )))
 }
 
-gen_stats(filelist, 0.2)
-
-
 genCorpus <- function(txtlist) {
   # text mining on sampled data
   cp <- VCorpus(VectorSource(txtlist))
@@ -161,9 +151,6 @@ genCorpus <- function(txtlist) {
   # inspect outcome
   #inspect( cp[1:2] ) # nolint: commented_code_linter.
 }
-
-
-corp <- genCorpus(ds)
 
 ngramGenerator <- function(corpus_object) {
   ## Modified
@@ -204,41 +191,6 @@ ngramGenerator <- function(corpus_object) {
   #rm(tdmr.t)
 }
 
-ngramGenerator(corpus_object = corp)
-
-
-# # regrouping by count
-# ngram2 <- ngram2 %>% group_by(w1,w2)             %>% summarise(cnt=sum(count))
-# ngram3 <- ngram3 %>% group_by(w1,w2,w3)          %>% summarise(cnt=sum(count))
-# ngram4 <- ngram4 %>% group_by(w1,w2,w3,w4)       %>% summarise(cnt=sum(count))
-# ngram5 <- ngram5 %>% group_by(w1,w2,w3,w4,w5)    %>% summarise(cnt=sum(count))
-# ngram6 <- ngram6 %>% group_by(w1,w2,w3,w4,w5,w6) %>% summarise(cnt=sum(count))
-# print(paste0("After regroping, in memory ", nrow(ngram6), " ", i, "-grams, taking: "))
-# print(object.size(ngram6), units='Mb')
-# 
-# 
-
-#paranoid memory cleanup
-#rm(cn)
-#rm(corp)
-#rm(i)
-
-# memory size of our initial corpus
-print(object.size(corp), units="Mb")
-
-# environment size (all n-grams including corpus)
-print(object.size(x=sapply(ls(), get)), units="Mb")
-
-# save image file to disk
-#rm(corp)
-#gc()
-# ngram1[,token:=paste(w1)]
-# ngram2[,token:=paste(w1, w2)]
-# ngram3[,token:=paste(w1, w2, w3)]
-# ngram4[,token:=paste(w1, w2, w3, w4)]
-# ngram5[,token:=paste(w1, w2, w3, w4, w5)]
-# ngram6[,token:=paste(w1, w2, w3, w4, w5, w6)]
-
 
 relative_freq <- function() {
   # calculate relative frequency
@@ -269,7 +221,7 @@ relative_freq <- function() {
 }
 
 # prepare these in advance, since shiny app load-time should be smallest possible
-ngram_stats <- data.frame(ngram = '', length = 0, count_min = 0, count_median = 0, count_mean = 0, count_max = 0, most_frequent_ngram='', mem = '')
+
 
 stat_generator <- function(startrange, endrange, stats_df){
 
@@ -292,22 +244,9 @@ stat_generator <- function(startrange, endrange, stats_df){
   return(stats_df)
 }
 
-ngram_stats <- stat_generator(1,6,ngram_stats)
-
-
-rm(s); rm(w);rm(m);rm(most_frequent_ngram);rm(i);
-ngram_stats <- ngram_stats[-1,]
-
-# save data for the Shiny app
-save.image("20th_corpus_freq.Rdata")
-
-
-#
-# predict function
-# simple back-off algorithm working its way from large 6-grams to tri/bi/unigrams
-#
 pred_words <- function(sentence, n = 5){
-  
+  # predict function
+# simple back-off algorithm working its way from large 6-grams to tri/bi/unigrams
   # follow a similar preparation path as the large corpus
   sentence <- removeNumbers(sentence)
   sentence <- removePunctuation(sentence)
@@ -361,6 +300,29 @@ pred_words <- function(sentence, n = 5){
 }
 
 
+
+
+
+
+zx <<- zipdownloader("https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip","Coursera-SwiftKey.zip", list = T)
+zip_test <<- ziphandler(zx)
+filedatareader(zip_test)
+filelist <<- c("en_US.blogs.txt", "en_US.twitter.txt", "en_US.news.txt")
+gen_stats(filelist, 0.2)
+corp <<- genCorpus(ds)
+ngramGenerator(corpus_object = corp)
+# memory size of our initial corpus
+print(object.size(corp), units="Mb")
+
+# environment size (all n-grams including corpus)
+print(object.size(x=sapply(ls(), get)), units="Mb")
+
+ngram_stats <<- data.frame(ngram = '', length = 0, count_min = 0, count_median = 0, count_mean = 0, count_max = 0, most_frequent_ngram='', mem = '')
+ngram_stats <<- stat_generator(1,6,ngram_stats)
+ngram_stats <<- ngram_stats[-1,]
+# save data for the Shiny app
+save.image("corpus_data.Rdata")
+
 pred_words('i want to go')
 pred_words('each')
 pred_words('thanks for all the')
@@ -372,246 +334,42 @@ pred_words('instead of saying this')
 pred_words('can you focus on the')
 
 
-# For each of the sentence fragments below use your natural language processing algorithm to predict the next word in the sentence.
-# 
-# When you breathe, I want to be the air for you. I'll be there for you, I'd live and I'd
-# 
-# die
-# 
-# sleep
-# 
-# give
-# 
-# eat
-# 1
-# point
-# 2. 
-# Guy at my table's wife got up to go to the bathroom and I asked about dessert and he started telling me about his
-# 
-# spiritual
-# 
-# financial
-# 
-# horticultural
-# 
-# marital
-# 1
-# point
-# 3. 
-# I'd give anything to see arctic monkeys this
-# 
-# morning
-# 
-# decade
-# 
-# month
-# 
-# weekend
-# 1
-# point
-# 4. 
-# Talking to your mom has the same effect as a hug and helps reduce your
-# 
-# sleepiness
-# 
-# hunger
-# 
-# happiness
-# 
-# stress
-# 1
-# point
-# 5. 
-# When you were in Holland you were like 1 inch away from me but you hadn't time to take a
-# 
-# look
-# 
-# walk
-# 
-# picture
-# 
-# minute
-# 1
-# point
-# 6. 
-# I'd just like all of these questions answered, a presentation of evidence, and a jury to settle the
-# 
-# incident
-# 
-# account
-# 
-# case
-# 
-# matter
-# 1
-# point
-# 7. 
-# I can't deal with unsymetrical things. I can't even hold an uneven number of bags of groceries in each
-# 
-# toe
-# 
-# arm
-# 
-# hand
-# 
-# finger
-# 1
-# point
-# 8. 
-# Every inch of you is perfect from the bottom to the
-# 
-# side
-# 
-# center
-# 
-# top
-# 
-# middle
-# 1
-# point
-# 9. 
-# I’m thankful my childhood was filled with imagination and bruises from playing
-# 
-# inside
-# 
-# daily
-# 
-# outside
-# 
-# weekly
-# 1
-# point
-# 10. 
-# I like how the same people are in almost all of Adam Sandler's
-# 
-# pictures
-# 
-# movies
-# 
-# novels
-# 
-# stories
 
 
 
+run_tasks <- function() {
+  if (!file.exists("corpus_data.Rdata")) {
+    zx <<- zipdownloader("https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip","Coursera-SwiftKey.zip", list = T)
+    zip_test <<- ziphandler(zx)
+    filedatareader(zip_test)
+    filelist <<- c("en_US.blogs.txt", "en_US.twitter.txt", "en_US.news.txt")
+    gen_stats(filelist, 0.2)
+    corp <<- genCorpus(ds)
+    ngramGenerator(corpus_object = corp)
+    # memory size of our initial corpus
+    print(object.size(corp), units="Mb")
 
+    # environment size (all n-grams including corpus)
+    print(object.size(x=sapply(ls(), get)), units="Mb")
+    
+    ngram_stats <<- data.frame(ngram = '', length = 0, count_min = 0, count_median = 0, count_mean = 0, count_max = 0, most_frequent_ngram='', mem = '')
+    ngram_stats <<- stat_generator(1,6,ngram_stats)
+    ngram_stats <<- ngram_stats[-1,]
+    # save data for the Shiny app
+    save.image("corpus_data.Rdata")
+  } else if (file.exists("corpus_data.Rdata")) {
+     load("corpus_data.Rdata", envir = .GlobalEnv)
+  }
+  
+  pred_words('i want to go')
+  pred_words('each')
+  pred_words('thanks for all the')
+  pred_words('according to')
+  pred_words('i have decided')
+  pred_words('at the top')
+  pred_words('it is by design')
+  pred_words('instead of saying this')
+  pred_words('can you focus on the')
+}
 
-# For each of the sentence fragments below use your natural language processing algorithm to predict the next word in the sentence.
-# 
-# The guy in front of me just bought a pound of bacon, a bouquet, and a case of
-# 
-# beer
-# 
-# cheese
-# 
-# soda
-# 
-# pretzels
-# 1
-# point
-# 2. 
-# You're the reason why I smile everyday. Can you follow me please? It would mean the
-# 
-# most
-# 
-# best
-# 
-# world
-# 
-# universe
-# 1
-# point
-# 3. 
-# Hey sunshine, can you follow me and make me the
-# 
-# bluest
-# 
-# smelliest
-# 
-# saddest
-# 
-# happiest
-# 1
-# point
-# 4. 
-# Very early observations on the Bills game: Offense still struggling but the
-# 
-# players
-# 
-# referees
-# 
-# crowd
-# 
-# defense
-# 1
-# point
-# 5. 
-# Go on a romantic date at the
-# 
-# beach
-# 
-# movies
-# 
-# mall
-# 
-# grocery
-# 1
-# point
-# 6. 
-# Well I'm pretty sure my granny has some old bagpipes in her garage I'll dust them off and be on my
-# 
-# motorcycle
-# 
-# phone
-# 
-# way
-# 
-# horse
-# 1
-# point
-# 7. 
-# Ohhhhh #PointBreak is on tomorrow. Love that film and haven't seen it in quite some
-# 
-# weeks
-# 
-# time
-# 
-# years
-# 
-# thing
-# 1
-# point
-# 8. 
-# After the ice bucket challenge Louis will push his long wet hair out of his eyes with his little
-# 
-# eyes
-# 
-# fingers
-# 
-# toes
-# 
-# ears
-# 1
-# point
-# 9. 
-# Be grateful for the good times and keep the faith during the
-# 
-# bad
-# 
-# hard
-# 
-# sad
-# 
-# worse
-# 1
-# point
-# 10. 
-# If this isn't the cutest thing you've ever seen, then you must be
-# 
-# asleep
-# 
-# insensitive
-# 
-# insane
-# 
-# callous
+run_tasks()
